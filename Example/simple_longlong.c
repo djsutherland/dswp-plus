842a1fa7743fdd2ee012e98716cf3fa7ed9a6c1d
zfy0701 <devnull@localhost>
2011-04-26 23:12:16 -0400

    more examples

---
+++ Example/simple_longlong.c
@@ -0,0 +1,9 @@
+
+long long fun(long long a) {
+	long long i, res;
+	res = 0;
+	for (i = 0; i < 100; i++) {
+		res = res + i;
+	}
+	return res;
+}
---
+++ Example/simpledouble.c
@@ -0,0 +1,10 @@
+
+double fun(double a) {
+	int i;
+	double res;
+	res = 0;
+	for (i = 0; i < 100; i++) {
+		res = res + i;
+	}
+	return res;
+}
